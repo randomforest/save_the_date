5aeb276ec807beb5433c303fdb72974f497c9d4f
etienne <etiennec@tpg.com.au>
2013-09-08 11:23:55 +0000

    Initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SaveTheDate::Application.config.secret_token = 'ffd269b754b8df1ac24400cf4ea06888125e1d9f243e3687f1be1aedbbb90ce1fd5a473c563f0f18f261cfd4f47a77c593b87d9abebe8168f63f1107ee6a044d'
